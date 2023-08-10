b63f555cbed62758ae97573f880a9c1b4f6020cf
Thibault <g-thibault@live.fr>
2023-08-10 16:28:36 +0200

    1.0

--- ranking.js
+++ ranking.js
@@ -15,28 +15,35 @@ rightCol.addEventListener('change', (event) => {
   }
 });
 
-const teams = document.querySelector('.teams');
-const rankingTable = document.querySelector('#rankingTable');
+const teams = document.querySelector('#teams');
+const ranking = document.querySelector('#ranking');
+const rankingInfo = document.getElementById('rankingInfo');
 
 function changeLeague(league) {
   teams.innerHTML = '';
-  rankingTable.innerHTML =
-    '<tbody><tr><td>...</td><td>...</td><td>...</td></tr></tbody>';
+  $('#ranking li').remove();
+  showRankingInfo();
+
+  if ($('#teams').hasClass('ui-sortable')) {
+    $('#teams, #ranking').sortable('destroy');
+  }
 
   if (league === 'noLeague') {
     teams.innerHTML = 'Select a League';
     return;
   }
 
-  for (const [key, value] of Object.entries(leagues[league])) {
-    const team = document.createElement('div');
+  createTeams(leagues[league]);
+}
+
+function createTeams(league) {
+  for (const [key, value] of Object.entries(league)) {
+    const team = document.createElement('li');
     team.setAttribute('id', key);
     team.setAttribute('class', 'team');
-    team.setAttribute('draggable', true);
-    team.setAttribute('ondragstart', 'drag(event)');
 
     const img = document.createElement('img');
-    img.setAttribute('src', `leagues/${value.logo}`);
+    img.setAttribute('src', `leagues/logo/${value.logo}`);
 
     const text = document.createElement('span');
     text.innerHTML = value.name;
@@ -45,52 +52,34 @@ function changeLeague(league) {
     team.appendChild(text);
     teams.appendChild(team);
   }
+
+  createSortable();
 }
 
-function allowDrop(ev) {
-  ev.preventDefault();
+function createSortable() {
+  $('#teams, #ranking').sortable({
+    connectWith: '.connectedSortable',
+    items: 'li',
+    update: function () {
+      showRankingInfo();
+    },
+  });
 }
 
-function drag(ev) {
-  let target = ev.target;
-  if (ev.target.tagName !== 'DIV') {
-    target = ev.target.parentNode;
+$('#ranking').on('click', 'li', function (event) {
+  $(event.target).closest('li').detach().appendTo('#teams');
+  showRankingInfo();
+});
+
+$('#teams').on('click', 'li', function (event) {
+  $(event.target).closest('li').detach().appendTo('#ranking');
+  showRankingInfo();
+});
+
+function showRankingInfo() {
+  if ($('#ranking li').length) {
+    rankingInfo.hidden = true;
+  } else {
+    rankingInfo.hidden = false;
   }
-  ev.dataTransfer.setData('name', target.id);
-}
-
-function drop(ev) {
-  const team = ev.dataTransfer.getData('name');
-  addTeamToRanking(team);
-}
-
-const rankingTableBody = document.querySelector('#rankingTable tbody');
-
-function addTeamToRanking(e) {
-  if (document.querySelector('#rankingTable td').innerHTML === '...') {
-    rankingTable.innerHTML = '';
-  }
-
-  document.querySelector(`[id="${e}"]`).remove();
-
-  const team = leagues[leagueSelection.value][e];
-
-  const tr = document.createElement('tr');
-
-  const position = document.createElement('td');
-  position.innerHTML = document.querySelectorAll('#rankingTable tr').length + 1;
-
-  const logo = document.createElement('td');
-  const img = document.createElement('img');
-  img.setAttribute('src', team.logo);
-  logo.appendChild(img);
-
-  const name = document.createElement('td');
-  name.innerHTML = team.name;
-
-  tr.appendChild(position);
-  tr.appendChild(logo);
-  tr.appendChild(name);
-
-  rankingTable.appendChild(tr);
 }
