ca74c1694ade2eadd26c6a1c93ff52a9b353abbc
Thibault <g-thibault@live.fr>
2023-08-10 20:45:30 +0200

    1.2

--- leagues/leagues.js
+++ leagues/leagues.js
@@ -199,4 +199,424 @@ const leagues = {
       },
     },
   },
+  'Premiere League': {
+    legends: {
+      'UEFA Champions League': {
+        color: 'blue',
+        position: [1, 2, 3, 4],
+      },
+      'UEFA Europa League': {
+        color: 'green',
+        position: [5],
+      },
+      Relegation: {
+        color: 'red',
+        position: [18, 19, 20],
+      },
+    },
+    teams: {
+      Arsenal: {
+        name: 'Arsenal',
+        logo: 'premiereleague/Arsenal.png',
+      },
+      AstonVilla: {
+        name: 'Aston Villa',
+        logo: 'premiereleague/AstonVilla.png',
+      },
+      Bournemouth: {
+        name: 'Bournemouth',
+        logo: 'premiereleague/Bournemouth.png',
+      },
+      Brentford: {
+        name: 'Brentford',
+        logo: 'premiereleague/Brentford.png',
+      },
+      Brighton: {
+        name: 'Brighton and Hove Albion',
+        logo: 'premiereleague/Brighton.png',
+      },
+      Burnley: {
+        name: 'Burnley',
+        logo: 'premiereleague/Burnley.png',
+      },
+      Chelsea: {
+        name: 'Chelsea',
+        logo: 'premiereleague/Chelsea.png',
+      },
+      CrystalPalace: {
+        name: 'Crystal Palace',
+        logo: 'premiereleague/CrystalPalace.png',
+      },
+      Everton: {
+        name: 'Everton',
+        logo: 'premiereleague/Everton.png',
+      },
+      Fulham: {
+        name: 'Fulham',
+        logo: 'premiereleague/Fulham.png',
+      },
+      Liverpool: {
+        name: 'Liverpool',
+        logo: 'premiereleague/Liverpool.png',
+      },
+      Luton: {
+        name: 'Luton Town',
+        logo: 'premiereleague/Luton.png',
+      },
+      ManCity: {
+        name: 'Manchester City',
+        logo: 'premiereleague/ManCity.png',
+      },
+      ManUnited: {
+        name: 'Manchester United',
+        logo: 'premiereleague/ManUnited.png',
+      },
+      Newcastle: {
+        name: 'Newcastle United',
+        logo: 'premiereleague/Newcastle.png',
+      },
+      Nottingham: {
+        name: 'Nottingham Forest',
+        logo: 'premiereleague/Nottingham.png',
+      },
+      SheffUtd: {
+        name: 'Sheffield United',
+        logo: 'premiereleague/SheffUtd.png',
+      },
+      Tottenham: {
+        name: 'Tottenham Hotspur',
+        logo: 'premiereleague/Tottenham.png',
+      },
+      WestHam: {
+        name: 'West Ham United',
+        logo: 'premiereleague/WestHam.png',
+      },
+      Wolverhampton: {
+        name: 'Wolverhampton Wanderers',
+        logo: 'premiereleague/Wolverhampton.png',
+      },
+    },
+  },
+  'League Championship': {
+    legends: {
+      Promotion: {
+        color: 'blue',
+        position: [1, 2],
+      },
+      'Promotion Play-off': {
+        color: 'green',
+        position: [3, 4, 5, 6],
+      },
+      Relegation: {
+        color: 'red',
+        position: [22, 23, 24],
+      },
+    },
+    teams: {
+      Birmingham: {
+        name: 'Birmingham City',
+        logo: 'leaguechampionship/Birmingham.png',
+      },
+      Blackburn: {
+        name: 'Blackburn Rovers',
+        logo: 'leaguechampionship/Blackburn.png',
+      },
+      BristolCity: {
+        name: 'Bristol City',
+        logo: 'leaguechampionship/BristolCity.png',
+      },
+      Cardiff: {
+        name: 'Cardiff City',
+        logo: 'leaguechampionship/Cardiff.png',
+      },
+      Coventry: {
+        name: 'Coventry City',
+        logo: 'leaguechampionship/Coventry.png',
+      },
+      Huddersfield: {
+        name: 'Huddersfield Town',
+        logo: 'leaguechampionship/Huddersfield.png',
+      },
+      Hull: {
+        name: 'Hull City',
+        logo: 'leaguechampionship/Hull.png',
+      },
+      Ipswich: {
+        name: 'Ipswich Town',
+        logo: 'leaguechampionship/Ipswich.png',
+      },
+      Leeds: {
+        name: 'Leeds United',
+        logo: 'leaguechampionship/Leeds.png',
+      },
+      Leicester: {
+        name: 'Leicester City',
+        logo: 'leaguechampionship/Leicester.png',
+      },
+      Middlesbrough: {
+        name: 'Middlesbrough',
+        logo: 'leaguechampionship/Middlesbrough.png',
+      },
+      Millwall: {
+        name: 'Millwall',
+        logo: 'leaguechampionship/Millwall.png',
+      },
+      Norwich: {
+        name: 'Norwich City',
+        logo: 'leaguechampionship/Norwich.png',
+      },
+      Plymouth: {
+        name: 'Plymouth Argyle',
+        logo: 'leaguechampionship/Plymouth.png',
+      },
+      Preston: {
+        name: 'Preston North End',
+        logo: 'leaguechampionship/Preston.png',
+      },
+      QPR: {
+        name: 'Queens Park Rangers',
+        logo: 'leaguechampionship/QPR.png',
+      },
+      Rotherham: {
+        name: 'Rotherham United',
+        logo: 'leaguechampionship/Rotherham.png',
+      },
+      SheffWed: {
+        name: 'Sheffield Wednesday',
+        logo: 'leaguechampionship/SheffWed.png',
+      },
+      Southampton: {
+        name: 'Southampton',
+        logo: 'leaguechampionship/Southampton.png',
+      },
+      Stoke: {
+        name: 'Stoke City',
+        logo: 'leaguechampionship/Stoke.png',
+      },
+      Sunderland: {
+        name: 'Sunderland',
+        logo: 'leaguechampionship/Sunderland.png',
+      },
+      Swansea: {
+        name: 'Swansea City',
+        logo: 'leaguechampionship/Swansea.png',
+      },
+      Watford: {
+        name: 'Watford',
+        logo: 'leaguechampionship/Watford.png',
+      },
+      WestBromwich: {
+        name: 'West Bromwich Albion',
+        logo: 'leaguechampionship/WestBromwich.png',
+      },
+    },
+  },
+  'La Liga': {
+    legends: {
+      'UEFA Champions League': {
+        color: 'blue',
+        position: [1, 2, 3, 4],
+      },
+      'UEFA Europa League': {
+        color: 'green',
+        position: [5],
+      },
+      'UEFA Conference League Qualifiers': {
+        color: 'orange',
+        position: [6],
+      },
+      Relegation: {
+        color: 'red',
+        position: [18, 19, 20],
+      },
+    },
+    teams: {
+      Alaves: {
+        name: 'Deportivo Alavés',
+        logo: 'laliga/Alaves.png',
+      },
+      Almeria: {
+        name: 'UD Almería',
+        logo: 'laliga/Almeria.png',
+      },
+      Atletico: {
+        name: 'Atlético de Madrid',
+        logo: 'laliga/Atletico.png',
+      },
+      Barcelone: {
+        name: 'FC Barcelona',
+        logo: 'laliga/Barcelone.png',
+      },
+      Betis: {
+        name: 'Real Betis',
+        logo: 'laliga/Betis.png',
+      },
+      Bilbao: {
+        name: 'Athletic Club',
+        logo: 'laliga/Bilbao.png',
+      },
+      Cadix: {
+        name: 'Cádiz CF',
+        logo: 'laliga/Cadix.png',
+      },
+      CeltaVigo: {
+        name: 'RC Celta',
+        logo: 'laliga/CeltaVigo.png',
+      },
+      Getafe: {
+        name: 'Getafe CF',
+        logo: 'laliga/Getafe.png',
+      },
+      Girona: {
+        name: 'Girona FC',
+        logo: 'laliga/Girona.png',
+      },
+      Grenade: {
+        name: 'Granada CF',
+        logo: 'laliga/Grenade.png',
+      },
+      LasPalmas: {
+        name: 'UD Las Palmas',
+        logo: 'laliga/LasPalmas.png',
+      },
+      Majorque: {
+        name: 'RCD Mallorca',
+        logo: 'laliga/Majorque.png',
+      },
+      Osasuna: {
+        name: 'CA Osasuna',
+        logo: 'laliga/Osasuna.png',
+      },
+      RealMadrid: {
+        name: 'Real Madrid',
+        logo: 'laliga/RealMadrid.png',
+      },
+      RealSociedad: {
+        name: 'Real Sociedad',
+        logo: 'laliga/RealSociedad.png',
+      },
+      Seville: {
+        name: 'Sevilla FC',
+        logo: 'laliga/Seville.png',
+      },
+      Valence: {
+        name: 'Valencia CF',
+        logo: 'laliga/Valence.png',
+      },
+      Vallecano: {
+        name: 'Rayo Vallecano',
+        logo: 'laliga/Vallecano.png',
+      },
+      Villarreal: {
+        name: 'Villarreal CF',
+        logo: 'laliga/Villarreal.png',
+      },
+    },
+  },
+  'Liga Adelante': {
+    legends: {
+      Promotion: {
+        color: 'blue',
+        position: [1, 2],
+      },
+      'Promotion Play-off': {
+        color: 'green',
+        position: [3, 4, 5, 6],
+      },
+      Relegation: {
+        color: 'red',
+        position: [19, 20, 21, 22],
+      },
+    },
+    teams: {
+      Albacete: {
+        name: 'Albacete',
+        logo: 'ligaadelante/Albacete.png',
+      },
+      Alcorcon: {
+        name: 'Alcorcón',
+        logo: 'ligaadelante/Alcorcon.png',
+      },
+      Amorebieta: {
+        name: 'Amorebieta',
+        logo: 'ligaadelante/Amorebieta.png',
+      },
+      Burgos: {
+        name: 'Burgos',
+        logo: 'ligaadelante/Burgos.png',
+      },
+      Carthagene: {
+        name: 'Cartagena',
+        logo: 'ligaadelante/Carthagene.png',
+      },
+      Eibar: {
+        name: 'Eibar',
+        logo: 'ligaadelante/Eibar.png',
+      },
+      Elche: {
+        name: 'Elche',
+        logo: 'ligaadelante/Elche.png',
+      },
+      Eldense: {
+        name: 'Eldense',
+        logo: 'ligaadelante/Eldense.png',
+      },
+      Espanyol: {
+        name: 'Espanyol',
+        logo: 'ligaadelante/Espanyol.png',
+      },
+      FCAndorra: {
+        name: 'Futbol Club Andorra',
+        logo: 'ligaadelante/FCAndorra.png',
+      },
+      Gijon: {
+        name: 'Sporting Gijón',
+        logo: 'ligaadelante/Gijon.png',
+      },
+      Huesca: {
+        name: 'Huesca',
+        logo: 'ligaadelante/Huesca.png',
+      },
+      Leganes: {
+        name: 'Leganés',
+        logo: 'ligaadelante/Leganes.png',
+      },
+      Levante: {
+        name: 'Levante',
+        logo: 'ligaadelante/Levante.png',
+      },
+      Mirandes: {
+        name: 'Mirandés',
+        logo: 'ligaadelante/Mirandes.png',
+      },
+      Oviedo: {
+        name: 'Real Oviedo',
+        logo: 'ligaadelante/Oviedo.png',
+      },
+      RacingFerrol: {
+        name: 'Ferrol',
+        logo: 'ligaadelante/RacingFerrol.png',
+      },
+      Santander: {
+        name: 'Santander',
+        logo: 'ligaadelante/Santander.png',
+      },
+      Saragosse: {
+        name: 'Saragosse',
+        logo: 'ligaadelante/Saragosse.png',
+      },
+      Tenerife: {
+        name: 'Tenerife',
+        logo: 'ligaadelante/Tenerife.png',
+      },
+      Valladolid: {
+        name: 'Valladolid',
+        logo: 'ligaadelante/Valladolid.png',
+      },
+      VillarrealII: {
+        name: 'Villarreal Club de Fútbol B',
+        logo: 'ligaadelante/VillarrealII.png',
+      },
+    },
+  },
 };
